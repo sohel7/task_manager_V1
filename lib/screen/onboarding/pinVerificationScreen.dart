ad2c17039df95df24a470465f1c6776758654e45
sohel7 <sohelocp@gmail.com>
2023-11-26 17:39:51 +0600

    first commit

---
+++ lib/screen/onboarding/pinVerificationScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class pinVerificationScreen extends StatefulWidget {
+  const pinVerificationScreen({super.key});
+
+  @override
+  State<pinVerificationScreen> createState() => _pinVerificationScreenState();
+}
+
+class _pinVerificationScreenState extends State<pinVerificationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/screen/onboarding/registrationScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class registrationScreen extends StatefulWidget {
+  const registrationScreen({super.key});
+
+  @override
+  State<registrationScreen> createState() => _registrationScreenState();
+}
+
+class _registrationScreenState extends State<registrationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
